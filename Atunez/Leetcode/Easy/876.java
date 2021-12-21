74800ce4c5c926623e0b127614417a6b47e29332
Abdel Issa <54098133+Atunez@users.noreply.github.com>
2021-12-21 04:52:50 -0500

    Create 876.java

---
+++ Atunez/Leetcode/Easy/876.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head.next == null){
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(fast != slow){
+            if(fast == null){
+                return slow;
+            }
+            
+            if(fast.next == null){
+                return slow.next;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+         }
+        return null;
+    }
+}
